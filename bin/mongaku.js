1e06d8d3d2515bb6bd0f6ca7b3b77e5f8219a3e2
John Resig <jeresig@gmail.com>
2016-07-30 23:14:31 -0400

    Add start/stop/restart commands to 'mongaku' binary.

--- bin/mongaku.js
+++ bin/mongaku.js
@@ -2,16 +2,60 @@
 
 "use strict";
 
+const path = require("path");
+
 const minimist = require("minimist");
-//const shell = require("shelljs");
+const shell = require("shelljs");
 
 const pkg = require("../package.json");
 
 const args = minimist(process.argv.slice(2));
 
-//const cmd = args._[0];
+const cmd = args._[0];
 
-if (args.h) {
+const localFile = (file) => path.resolve(__dirname, file);
+const getBinary = (bin) => {
+    const binPath = localFile(`../node_modules/.bin/${bin}`);
+
+    if (!shell.which(binPath)) {
+        console.error(`${bin} not found. Please run 'npm install'.`);
+        process.exit(1);
+    }
+
+    return binPath;
+};
+
+if (args.v || args.version) {
+    console.log(pkg.version);
+
+} else if (cmd === "start") {
+    process.env.NODE_ENV = "production";
+
+    const basePath = args.logs || "";
+    const serverLog = path.resolve(basePath, "mongaku-server.log");
+    const stdoutLog = path.resolve(basePath, "mongaku-stdout.log");
+    const stderrLog = path.resolve(basePath, "mongaku-stderr.log");
+    const serverjs = localFile("../server.js");
+
+    const startCmd = [
+        getBinary("naught"),
+        "start",
+        "--worker-count 2",
+        `--log ${serverLog}`,
+        `--stdout ${stdoutLog}`,
+        `--stderr ${stderrLog}`,
+        serverjs,
+    ].join(" ");
+
+    shell.exec(startCmd);
+
+} else if (cmd === "stop") {
+    shell.exec(`${getBinary("naught")} stop`);
+
+} else if (cmd === "restart") {
+    shell.exec(`${getBinary("naught")} deploy`);
+
+} else {
     console.log(`${pkg.name}: ${pkg.description}
 
 usage: mongaku <command>
@@ -19,6 +63,7 @@ usage: mongaku <command>
 Commands:
     install
     start
+      --logs
     stop
     restart
 
@@ -26,6 +71,4 @@ Commands:
 -h: Show available commands
 `);
 
-} else if (args.v || args.version) {
-    console.log(pkg.version);
 }
