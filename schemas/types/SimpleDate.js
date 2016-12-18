acd756262366dab3d7d4cd4d8a4cbb0e79a6a132
John Resig <jeresig@gmail.com>
2016-12-17 20:19:34 -0500

    Bring in missing SimpleDate files.

---
+++ schemas/types/SimpleDate.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const React = require("react");
+
+const FixedStringDisplay = React.createFactory(
+    require("../../views/types/view/FixedString.jsx"));
+const SimpleDateEdit = React.createFactory(
+    require("../../views/types/edit/SimpleDate.jsx"));
+
+const SimpleDate = function(options) {
+    this.options = options;
+    /*
+    name
+    type
+    searchName
+    title(i18n)
+    placeholder(i18n)
+    multiple: Bool
+    recommended: Bool
+    hidden: Bool
+    */
+};
+
+SimpleDate.prototype = {
+    searchName() {
+        return this.options.searchName || this.options.name;
+    },
+
+    value(query) {
+        return query[this.searchName()];
+    },
+
+    fields(value) {
+        return {[this.searchName()]: value};
+    },
+
+    renderView(value) {
+        return FixedStringDisplay({
+            name: this.options.name,
+            type: this.options.type,
+            value,
+            multiline: this.options.multiline,
+        });
+    },
+
+    renderEdit(value) {
+        return SimpleDateEdit({
+            name: this.options.name,
+            type: this.options.type,
+            value,
+            multiline: this.options.multiline,
+            hidden: this.options.hidden,
+        });
+    },
+
+    schema() {
+        const type = {
+            type: Date,
+            es_indexed: true,
+            recommended: !!this.options.recommended,
+        };
+
+        return this.options.multiple ? [type] : type;
+    },
+};
+
+module.exports = SimpleDate;
---
+++ views/types/edit/SimpleDate.jsx
@@ -0,0 +1,28 @@
+"use strict";
+
+const React = require("react");
+
+const SimpleDateEdit = React.createClass({
+    propTypes: {
+        name: React.PropTypes.string.isRequired,
+        value: React.PropTypes.instanceOf(Date),
+    },
+
+    render() {
+        let dateString = "";
+
+        if (this.props.value) {
+            dateString = this.props.value.toISOString()
+                .replace(/T.*$/, "");
+        }
+
+        return <input
+            name={this.props.name}
+            type="date"
+            className="form-control"
+            defaultValue={dateString}
+        />;
+    },
+});
+
+module.exports = SimpleDateEdit;
