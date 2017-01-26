8534df17f67d29b85ad2fc94525dfa3f6ea4e7d3
John Resig <jeresig@gmail.com>
2017-01-26 00:01:43 -0500

    Add missing Wrapper/types/react-views files.

---
+++ views/Wrapper.jsx
@@ -0,0 +1,112 @@
+// @flow
+
+const qs = require("querystring");
+
+const moment = require("moment");
+const React = require("react");
+
+const urls = require("../lib/urls");
+import type {User} from "./types.jsx";
+
+class Wrapper extends React.Component {
+    getChildContext() {
+        const {originalUrl, user, lang, gettext, format} = this.props;
+
+        return {
+            lang,
+            gettext,
+            format,
+
+            getOtherURL(locale: string): string {
+                return urls.gen(locale, originalUrl);
+            },
+
+            URL(path: string | {getURL: (lang: string) => string},
+                    query?: Object): string {
+                let url = typeof path.getURL === "function" ?
+                    path.getURL(lang) :
+                    urls.gen(lang, path);
+
+                if (query) {
+                    url = url + (url.indexOf("?") >= 0 ? "&" : "?") +
+                        qs.stringify(query);
+                }
+
+                return url;
+            },
+
+            fullName(item: {getFullName: (lang: string) => string} |
+                    {name: string} | string): string {
+                return typeof item.getFullName === "function" ?
+                    item.getFullName(lang) :
+                    typeof item.name === "string" ?
+                        item.name :
+                        typeof item === "string" ?
+                            item :
+                            "";
+            },
+
+            shortName(item: {getShortName: (lang: string) => string}): string {
+                return item.getShortName(lang);
+            },
+
+            getTitle(item: {getTitle: () => string}): string {
+                return item.getTitle({lang, gettext, format});
+            },
+
+            getShortTitle(item: {getShortTitle: () => string}): string {
+                return item.getShortTitle({lang, gettext, format});
+            },
+
+            // Format a number using commas
+            stringNum(num: number): string {
+                // TODO(jeresig): Have a better way to handle this.
+                const separator = lang === "en" ? "," : ".";
+                const result = (typeof num === "number" ? num : "");
+                return result.toString().replace(/\B(?=(\d{3})+(?!\d))/g,
+                    separator);
+            },
+
+            relativeDate(date: Date): string {
+                return moment(date).locale(lang).fromNow();
+            },
+
+            fixedDate(date: Date): string {
+                return moment(date).locale(lang).format("LLL");
+            },
+
+            currentUser: (): ?User => user,
+        };
+    }
+
+    props: {
+        originalUrl?: string,
+        user?: User,
+        lang: string,
+        gettext: (text: string) => string,
+        format: (text: string, options: {}) => string,
+        children?: React.Element<*>,
+    }
+
+    render() {
+        return this.props.children;
+    }
+}
+
+Wrapper.childContextTypes = {
+    lang: React.PropTypes.string,
+    gettext: React.PropTypes.func,
+    format: React.PropTypes.func,
+    getOtherURL: React.PropTypes.func,
+    URL: React.PropTypes.func,
+    fullName: React.PropTypes.func,
+    shortName: React.PropTypes.func,
+    getTitle: React.PropTypes.func,
+    getShortTitle: React.PropTypes.func,
+    stringNum: React.PropTypes.func,
+    relativeDate: React.PropTypes.func,
+    fixedDate: React.PropTypes.func,
+    currentUser: React.PropTypes.func,
+};
+
+module.exports = Wrapper;
---
+++ views/types.jsx
@@ -0,0 +1,25 @@
+// @flow
+
+// Methods and properties defined in view-methods.js and i18n.js
+export type Context = {
+    lang: string,
+    URL: (path: string | {getURL: (lang: string) => string}) => string,
+    getOtherURL: (lang: string) => string,
+    fullName: (name: *) => string,
+    shortName: (name: *) => string,
+    getTitle: (item: {getTitle: () => string}) => string,
+    getShortTitle: (item: {getShortTitle: () => string}) => string,
+    stringNum: (num: number) => string,
+    fixedDate: (date: Date) => string,
+    relativeDate: (date: Date) => string,
+    currentUser: () => ?Object,
+    gettext: (text: string) => string,
+    format: (text: string, options: {}) => string,
+};
+
+// From User.js
+export type User = {
+    email: string,
+    sourceAdmin: Array<string>,
+    siteAdmin: boolean,
+};
