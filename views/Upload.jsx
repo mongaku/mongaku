7870782dff4f2d10934278e0a1ce0e121b06edf1
John Resig <jeresig@gmail.com>
2017-01-25 21:20:50 -0500

    Rewrite Upload.jsx.

--- views/Upload.jsx
+++ views/Upload.jsx
@@ -4,116 +4,145 @@ const React = require("react");
 
 const Page = require("./Page.jsx");
 
-const Upload = React.createClass({
-    propTypes: {
-        URL: React.PropTypes.func.isRequired,
-        format: React.PropTypes.func.isRequired,
-        fullName: React.PropTypes.func.isRequired,
-        getTitle: React.PropTypes.func.isRequired,
-        gettext: React.PropTypes.func.isRequired,
-        image: React.PropTypes.shape({
-            getOriginalURL: React.PropTypes.func.isRequired,
-            getScaledURL: React.PropTypes.func.isRequired,
-        }).isRequired,
-        shortName: React.PropTypes.func.isRequired,
-        similar: React.PropTypes.arrayOf(
-            React.PropTypes.shape({
-                record: React.PropTypes.shape({
-                    _id: React.PropTypes.string.isRequired,
-                    source: React.PropTypes.string.isRequired,
-                }).isRequired,
-                score: React.PropTypes.number.isRequired,
-            })
-        ).isRequired,
-    },
+type ImageType = {
+    _id: string,
+    getOriginalURL: () => string,
+    getScaledURL: () => string,
+    getThumbURL: () => string,
+};
 
-    renderUpload() {
-        const title = this.props.gettext("Uploaded Image");
+type RecordType = {
+    _id: string,
+    type: string,
+    url: string,
+    images: Array<ImageType>,
+    getOriginalURL: () => string,
+    getThumbURL: () => string,
+    getTitle: () => string,
+    getSource: () => Source,
+    getURL: (lang: string) => string,
+};
 
-        return <div className="panel panel-default">
-            <div className="panel-heading">
-                <strong>{this.props.gettext("Uploaded Image")}</strong>
-            </div>
-            <div className="panel-body">
-                <a href={this.props.image.getOriginalURL()}>
-                    <img src={this.props.image.getScaledURL()}
-                        alt={title}
-                        title={title}
-                        className="img-responsive center-block"
-                    />
+type Source = {
+    _id: string,
+    name: string,
+    getURL: (lang: string) => string,
+};
+
+type MatchType = {
+    _id: string,
+    recordModel: RecordType,
+    score: number,
+};
+
+type Props = {
+    // GlobalProps
+    URL: (path: string | {getURL: (lang: string) => string}) => string,
+    gettext: (text: string) => string,
+    lang: string,
+    getTitle: (item: {getTitle: () => string}) => string,
+    format: (text: string, options: {}) => string,
+    fullName: (name: *) => string,
+    shortName: (name: *) => string,
+
+    image: ImageType,
+    similar: Array<MatchType>,
+};
+
+const UploadedImage = ({
+    gettext,
+    image,
+}: Props) => {
+    const title = gettext("Uploaded Image");
+
+    return <div className="panel panel-default">
+        <div className="panel-heading">
+            <strong>{gettext("Uploaded Image")}</strong>
+        </div>
+        <div className="panel-body">
+            <a href={image.getOriginalURL()}>
+                <img src={image.getScaledURL()}
+                    alt={title}
+                    title={title}
+                    className="img-responsive center-block"
+                />
+            </a>
+        </div>
+    </div>;
+};
+
+const Match = ({
+    match: {recordModel, score},
+    getTitle,
+    URL,
+    format,
+    gettext,
+    fullName,
+    shortName,
+}: Props & {match: MatchType}) => {
+    const source = recordModel.getSource();
+
+    return <div className="img col-md-6 col-sm-4 col-xs-6">
+        <div className="img-wrap">
+            <a href={URL(recordModel)}>
+                <img src={recordModel.getThumbURL()}
+                    alt={getTitle(recordModel)}
+                    title={getTitle(recordModel)}
+                    className="img-responsive center-block"
+                />
+            </a>
+        </div>
+        <div className="details">
+            <div className="wrap">
+                <span>{format(gettext("Score: %(score)s"),
+                    {score: score})}</span>
+
+                <a className="pull-right"
+                    href={URL(source)}
+                    title={fullName(source)}
+                >
+                    {shortName(source)}
                 </a>
             </div>
+        </div>
+    </div>;
+};
+
+const Results = (props: Props) => {
+    const {gettext, similar} = props;
+
+    let similarResults;
+
+    if (similar.length === 0) {
+        similarResults = <div className="col-xs-12">
+            <p>{gettext("No similar images were found.")}</p>
         </div>;
-    },
+    } else {
+        similarResults = similar.map((match) =>
+            <Match {...props} match={match} key={match.recordModel._id} />);
+    }
 
-    renderResults() {
-        return <div className="panel panel-default">
-            <div className="panel-heading">
-                <strong>{this.props.gettext("Similar Images")}</strong>
+    return <div className="panel panel-default">
+        <div className="panel-heading">
+            <strong>{gettext("Similar Images")}</strong>
+        </div>
+        <div className="panel-body row">
+            {similarResults}
+        </div>
+    </div>;
+};
+
+const Upload = (props: Props) => {
+    return <Page {...props}>
+        <div className="row">
+            <div className="col-md-6">
+                <UploadedImage {...props} />
             </div>
-            <div className="panel-body row">
-                {this.renderSimilar(this.props.similar)}
+            <div className="col-md-6">
+                <Results {...props} />
             </div>
-        </div>;
-    },
-
-    renderSimilar(similar) {
-        if (similar.length === 0) {
-            return <div className="col-xs-12">
-                <p>{this.props.gettext("No similar images were found.")}</p>
-            </div>;
-        }
-
-        return similar.map((match) => this.renderMatch(match));
-    },
-
-    renderMatch(match) {
-        const record = match.recordModel;
-        const source = record.getSource();
-
-        return <div className="img col-md-6 col-sm-4 col-xs-6"
-            key={record._id}
-        >
-            <div className="img-wrap">
-                <a href={this.props.URL(record)}>
-                    <img src={record.getThumbURL()}
-                        alt={this.props.getTitle(record)}
-                        title={this.props.getTitle(record)}
-                        className="img-responsive center-block"
-                    />
-                </a>
-            </div>
-            <div className="details">
-                <div className="wrap">
-                    <span>{this.props.format(
-                        this.props.gettext("Score: %(score)s"),
-                            {score: match.score})}</span>
-
-                    <a className="pull-right"
-                        href={this.props.URL(source)}
-                        title={this.props.fullName(source)}
-                    >
-                        {this.props.shortName(source)}
-                    </a>
-                </div>
-            </div>
-        </div>;
-    },
-
-    render() {
-        return <Page
-            {...this.props}
-        >
-            <div className="row">
-                <div className="col-md-6">
-                    {this.renderUpload()}
-                </div>
-                <div className="col-md-6">
-                    {this.renderResults()}
-                </div>
-            </div>
-        </Page>;
-    },
-});
+        </div>
+    </Page>;
+};
 
 module.exports = Upload;
