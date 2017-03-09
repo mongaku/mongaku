27f4d40f6a1283f67c30f0406d89ca6540c633d8
John Resig <jeresig@gmail.com>
2017-03-09 01:15:26 -0500

    Fix npm run dev-test.

--- src/tests/server.js
+++ src/tests/server.js
@@ -1,11 +1,14 @@
 const init = require("./init");
+const {mockFS} = init;
 
 init.init((err) => {
     if (err) {
         console.error(err);
     } else {
-        console.log("STARTED");
-        const cron = require("../server/cron");
-        cron.start();
+        mockFS(() => {
+            console.log("STARTED");
+            const cron = require("../server/cron");
+            cron.start();
+        });
     }
 });
